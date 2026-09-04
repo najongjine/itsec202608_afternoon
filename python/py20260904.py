9b101c161c7be2d41dc9c9b62ce459250d224950
najongjine <najongjin3@hotmail.com>
2026-09-04 15:21:11 +0900

    .

--- python/py20260904.py
+++ python/py20260904.py
@@ -15,5 +15,12 @@ person={
     ,"age":30
     ,"city":"서울"
 }
+# key 만 i에 담김
 for i in person:
     print(f"i:{i}")
+# value만 i에 담김
+for i in person.values():
+    print(f"i:{i}")
+# key는 i, value는 v에 담김
+for i,v in person.items():
+    print(f"i:{i}, v:{v}")
