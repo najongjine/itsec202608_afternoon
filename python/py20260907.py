0b8b61b8e9fcf7ade96c7921673a661f8e8047b3
najongjine <najongjin3@hotmail.com>
2026-09-07 15:22:30 +0900

    .

--- python/py20260907.py
+++ python/py20260907.py
@@ -1,6 +1,10 @@
-
+# 버블정렬
 a=[1,9,7,6]
-for index in range(len(a)-1):
-    if a[index] > a[index+1]:
-        a[index],a[index+1]=a[index+1],a[index]
+
+for d in range(len(a)-1):
+    for index in range(len(a)-1):
+        if a[index] > a[index+1]:
+            a[index],a[index+1]=a[index+1],a[index]
+
+
 print(f"a:{a}")
