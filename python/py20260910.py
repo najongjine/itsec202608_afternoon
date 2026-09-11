f57642bc0a9544559a9c01aa278f523bf67002dd
najongjine <najongjin3@hotmail.com>
2026-09-11 15:23:45 +0900

    .

--- python/py20260910.py
+++ python/py20260910.py
@@ -7,8 +7,16 @@
 
 #랜덤숫자 만들기. 1~45
 import random
-nums=[]
-for i in range(6):
-    x=random.randint(1,46)
-    nums.append(x)
+nums=set()
+for i in range(9999):
+    x=random.randint(1,45)
+    nums.add(x)
+    if len(nums) >= 6:
+        break
+"""
+set는 중복을 자동으로 제거해서 편하지만, 순서개념이 없어요
+list(배열)은 중복 재거 개념이 없지만, 순서 개념이 있어요
+"""
+nums=list(nums)
+nums.sort()
 print(f"nums:{nums}")
